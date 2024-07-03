2d7447a26a4001465cf48c0d1229625a387d1cb4
Dilshodjon <dilshodjonnorbekov@gmail.com>
2024-07-03 19:29:45 +0500

    gunicorn install

--- KINOMIR_BOT/settings.py
+++ KINOMIR_BOT/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0%&61!ik%%9xe@a!asmr+-z5&@%b*%8pon5oab3#2#dsoa^tmr'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
