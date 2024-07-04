eff1992431aa996bb45271513546e7ab21ba5bae
Dilshodjon <dilshodjonnorbekov@gmail.com>
2024-07-04 09:40:36 +0500

    images_charfield

--- cinema/models.py
+++ cinema/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class PartModels(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name="ID")
-    photo = models.ImageField(verbose_name='Rasmi', upload_to='part_photos/')
+    photo = models.CharField(verbose_name='Rasmi', max_length=255)
     title = models.CharField(max_length=255, verbose_name="Sarlavha")
     caption = models.TextField(verbose_name="Izoh")
     movie = models.ForeignKey('MovieModels', on_delete=models.CASCADE, related_name='part', verbose_name="Kino")
@@ -21,7 +21,7 @@ class PartModels(models.Model):
 
 class SeasonModels(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name="ID")
-    photo = models.ImageField(upload_to='season_photos/', verbose_name="Rasmi")
+    photo = models.CharField(max_length=255, verbose_name="Rasmi")
     title = models.CharField(max_length=255, verbose_name="Sarlavha")
     caption = models.TextField(verbose_name="Izoh")
     season = models.ForeignKey(PartModels, on_delete=models.CASCADE, related_name='season', verbose_name="Fasl")
@@ -46,7 +46,7 @@ class MovieModels(models.Model):
     file_id = models.CharField(max_length=255)
     title = models.CharField(max_length=255, verbose_name="Sarlavha")
     caption = models.TextField(verbose_name="Izoh")
-    photo = models.ImageField(max_length=255, null=True, blank=True, upload_to='kino_photos/', verbose_name="Rasmi")
+    photo = models.CharField(max_length=255, null=True, blank=True, verbose_name="Rasmi")
     category = models.CharField(max_length=25, choices=Categories, default='primieras', verbose_name="Kategoriyasi")
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
